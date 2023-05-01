15220e459f6f1c330ebf7853b78207ecdf67e8b6
zyguel <123135343+zyguel@users.noreply.github.com>
2023-05-01 19:07:41 +0800

    Update EX#1.php

--- EX#1.php
+++ EX#1.php
@@ -4,9 +4,10 @@
     <title>PHP Hello World</title>
   </head>
   <body>
+    <h1 id="php">hi</h1>
     <?php
       $text = "Hello World!";
-      echo "<h1>$text</h1>";
+      echo "<script>document.getElementById('php').innerHTML = '$text';</script>";
     ?>
   </body>
 </html>
