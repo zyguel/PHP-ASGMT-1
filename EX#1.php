47c46b19ee67befe3268db31408b8bb9e886f669
zyguel <123135343+zyguel@users.noreply.github.com>
2023-05-01 19:04:33 +0800

    Update EX#1.php

--- EX#1.php
+++ EX#1.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Hello World</title>
+  </head>
+  <body>
+    <?php
+      $text = "Hello World!";
+      echo "<h1>$text</h1>";
+    ?>
+  </body>
+</html>
